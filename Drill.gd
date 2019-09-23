302ccd5f10379791f5123f5bc9bf82c21623e0ea
Raphael Yammouni <ryammouni@gmail.com>
2019-09-23 21:38:01 +0200

    First commit

---
+++ Drill.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export var durability = 100;
+export var strength = 100;
+
+func _ready():
+	pass
---
+++ Level.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+export var distance = 0;
+export var speed = 1.0;
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
